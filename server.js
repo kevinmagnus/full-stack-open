6322ba998d65204b3a378fafaa58dc6ea564e799
Chigemezu Nathaniel Emmanuel <chigemezuemmanuel641@gmail.com>
2025-11-15 15:53:59 +0100

    Update server.js. Changed app.use(express) extended value to 'false'

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const __dirname = path.resolve();
 app.use(cors({origin: '*'}));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: false}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
