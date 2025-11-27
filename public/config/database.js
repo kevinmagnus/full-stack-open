c08b06d68187a1e1543cc839b26b3720d7f247d2
Chigemezu Nathaniel Emmanuel <chigemezuemmanuel641@gmail.com>
2025-11-27 11:16:42 +0100

    Update database.js

--- public/config/database.js
+++ public/config/database.js
@@ -1,37 +1,7 @@
-/* import mongoose from 'mongoose';
-
-const connectDB = async () => {
-
-
-    try {
-
-
-        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-
-
-        console.log(`\n MongoDB connected successfully!`);
-
-    }catch(error) {
-
-        console.log("MongoDB connection failed", error);
-
-        process.exit(1);
-
-
-    }
-    
-}
-
-export default connectDB;
-
-*/
-
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
+    
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
