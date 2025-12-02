a205b0eb2d3379f81a268a82a6ea9506c51c7fe7
Chigemezu Emmanuel <chigemezuemmanuel641@gmail.com>
2025-12-02 17:16:11 +0100

    email not sending correction

--- services/emailService.js
+++ services/emailService.js
@@ -9,7 +9,7 @@ const createTransporter = () => {
     tls: {
 rejectUnauthorized: false,
     },
-    connectionTimeout: 30000,
+    connectionTimeout: 10000,
      // or 'smtp.gmail.com'
     auth: {
       user: process.env.EMAIL_USER,
