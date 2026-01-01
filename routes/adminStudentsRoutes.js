74a6739bc4463c060f368ff898762966f7b22cfd
Chigemezu Emmanuel <chigemezuemmanuel641@gmail.com>
2026-01-01 10:29:39 +0100

    added commit

--- routes/adminStudentsRoutes.js
+++ routes/adminStudentsRoutes.js
@@ -6,10 +6,10 @@ import authenticateAdmin from "../controllers/adminAuthController.js";
 const router = express.Router();
 
 //router to render get all students page.
-router.get('/api/get-all-students-data', getStudentsDataPage);
+router.get('/api/get-all-students-data',authenticateAdmin, getStudentsDataPage);
 
 //router to get handle all students
-router.get('/api/display-all-students-data', getAllStudentsData);
+router.get('/api/display-all-students-data',authenticateAdmin, getAllStudentsData);
 
 
 
